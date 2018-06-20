2d39bf33f57b31e4187e7354fe3799bf63084903
JOBFER <gbringashotmail@hotmail.com>
2018-06-20 19:29:51 +0200

    Modificaciones Persistence Unit

--- src/java/dao/CategoriaFacade.java
+++ src/java/dao/CategoriaFacade.java
@@ -6,7 +6,9 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 @TransactionManagement(TransactionManagementType.CONTAINER)
 @Stateless
@@ -17,22 +19,31 @@ public class CategoriaFacade extends AbstractFacade<Categoria> {
 
     @PersistenceContext(unitName = "EjemploBD_JTA_JNDI_CMTPU")
     private EntityManager em;
+    
+//    //Tb se permite asi:
+//    @PersistenceUnit(unitName = "EjemploBD_JTA_JNDI_CMTPU")
+//    private EntityManagerFactory emf;
 
     @Override
     protected EntityManager getEntityManager() {
         return em;
+//        return em = emf.createEntityManager();
     }
     
     public void crearCategoria(){
         
         System.out.println("*****************************  principal  *******************************");
+//        EntityManager em = getEntityManager();
+        
+        em.persist(new Categoria("miCategoria"));
+//        em.persist(new Categoria(1)); //Esto para provocar excepcion y ROLLBACK   
+        
         try{
             eJBSubordinado.subordinado();
         }catch(Exception e){
             System.out.println("Excepcion en principal: " + e);
         }
-        em.persist(new Categoria("miCategoria"));
-//        em.persist(new Categoria(1)); //Esto para provocar excepcion y ROLLBACK        
+     
     }
 
     public CategoriaFacade() {
--- src/java/dao/EJBSubordinado.java
+++ src/java/dao/EJBSubordinado.java
@@ -12,7 +12,9 @@ import javax.ejb.TransactionAttributeType;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 @Stateless
 @TransactionManagement(TransactionManagementType.CONTAINER)
@@ -20,6 +22,10 @@ public class EJBSubordinado {
     
     @PersistenceContext(unitName = "EjemploBD_JTA_JNDI_CMTPU")
     private EntityManager em;
+    
+//    //Tb se permite así
+//    @PersistenceUnit(unitName = "EjemploBD_JTA_JNDI_CMTPU")
+//    private EntityManagerFactory emf;
 
 //    @TransactionAttribute(value = TransactionAttributeType.NEVER)  //Tb se puede poner en la clase
 //    @TransactionAttribute(value = TransactionAttributeType.REQUIRED)    
@@ -27,8 +33,10 @@ public class EJBSubordinado {
 //    @TransactionAttribute(value = TransactionAttributeType.MANDATORY)
     public void subordinado() {
         
+//        em = emf.createEntityManager();
+        
         System.out.println("*****************************  subordinado  *******************************");
-//        em.persist(new Categoria("miCategoriaSubordinada"));
-        em.persist(new Categoria(1));
+        em.persist(new Categoria("miCategoriaSubordinada"));
+//        em.persist(new Categoria(1));
     }
 }
